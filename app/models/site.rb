8c1824fe9d5f887fc6b2a7416afcfde117f3d8a6
Denis Jacquemin <denis.jacquemin@gmail.com>
2013-03-19 08:03:38 +0100

    hmanize Site's id and validations

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,6 +1,11 @@
 class Site
   include Mongoid::Document
+  
   field :n, as: :name, type: String
+  field :_id, type: String, default: ->{ name.to_s.parameterize }
   
   has_many :lists
+  
+  validates_presence_of :n
+  validates_uniqueness_of :n
 end
